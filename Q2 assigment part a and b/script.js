906066a9fb4cef807e7e577934dc956c84d6dcc8
RohailJavaid786 <155910153+RohailJavaid786@users.noreply.github.com>
2024-01-22 05:21:30 -0800

    Add files via upload

---
+++ Q2 assigment part a and b/script.js
@@ -0,0 +1,18 @@
+
+
+    //Q2 a//
+
+var length = 20;
+var width = 8;
+
+var area = length * width;
+
+console.log("The area of the rectangle is: " + area);
+    
+      
+
+          //Q2 b//
+
+var userName = prompt("What is your name?");
+
+alert("Hello, " + userName + "! Welcome!");
